38a635f7b3197b59ade18ca2625d06fc8c043af4
Id_Programming <156506190+idprogramming8@users.noreply.github.com>
2024-05-26 15:29:27 +0300

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{  
+    int num1;
+    int num2;
+    cout << "Введите первое число:";
+    cin >> num1;
+    cout << "Введите второе число:";
+    cin >> num2;
+    cout << num1 << "+" << num2 << "=" << num1 + num2;
+    return 0;
+}  
